2483ff7254979835fafda6271c10799014092555
Tristan Emmerson <tristan@stickerslug.com>
2020-06-09 15:12:48 -0700

    Add notes and base loops to build/learn from from youtube course.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 
+//we check all values, starting with an index of 1 and incrementing by 1 again <=5. 
+//For all those values that the remainder of dividing those values by 2 is 0 (meaning they are even), display that output in console
+for (let i =1; i <= 5; i++) {
+  if (i % 2 !== 0) console.log(i);
+}
 
-for (let i =1; i < 5; i++) {
-  console.log("hello world", i, i, i, i);
+//same function as above. just decremementing. will output reversal of ^
+for (let i =5; i >= 1; i--) {
+  if (i % 2 !== 0) console.log(i);
 }
